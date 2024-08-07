abb04ce17152fef1494a1f2424ee58dd28fca11e
imane <ibelhaj962@gmail.com>
2024-08-07 16:50:12 +0100

    Data Preprocessing completed, adding darija profanity, created final csv

--- data_processing/merging_csvs/marge_cleaned_csvs.py
+++ data_processing/merging_csvs/marge_cleaned_csvs.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # Load the two CSV files
-df1 = pd.read_csv('standardized_all.csv')
-df2 = pd.read_csv('updated_bank.csv')
+df1 = pd.read_csv('merged_final.csv')
+df2 = pd.read_csv('processed_darija_dataset.csv')
 
 # Concatenate the DataFrames
 merged_df = pd.concat([df1, df2], ignore_index=True)
 
 # Save the merged DataFrame to a new CSV file
-merged_df.to_csv('merged_final.csv', index=False)
+merged_df.to_csv('final.csv', index=False)
 
 print("Merged CSV file created successfully.")
